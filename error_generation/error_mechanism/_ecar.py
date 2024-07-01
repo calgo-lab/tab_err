298aa07903391fe99829c7fc72522c29f1de9ec2
Philipp Jung <philippjung@posteo.de>
2024-07-01 17:27:51 +0200

    Add Mid-Level API (#7)
    This commit adds the mid-level API to `error_generation`. It can be used
    to apply multiple triples of (error_mechanism, error_type, error_rate)
    to a single table and manages conflicts that can emerge in the scenario.
    
    I'm not super happy with the implementation - ideally, it would
    internally use the low-level API, which it currently is not. That's not
    so elegant. However, the current approach works, and I'm out of time to
    make things nicer.

--- error_generation/error_mechanism/_ecar.py
+++ error_generation/error_mechanism/_ecar.py
@@ -15,15 +15,22 @@ if TYPE_CHECKING:
 
 class ECAR(ErrorMechanism):
     def _sample(self: ECAR, data: pd.DataFrame, column: str | int, error_rate: float, error_mask: pd.DataFrame) -> pd.DataFrame:
-        se_data = get_column(data, column)
+        se_data = get_column(data, column)  # noqa: F841
         se_mask = get_column(error_mask, column)
+
+        se_mask_error_free = se_mask[~se_mask]
 
         if self.condition_to_column is not None:
             warnings.warn("'condition_to_column' is set but will be ignored by ECAR.", stacklevel=1)
 
-        n_errors = int(se_data.size * error_rate)
+        n_errors = int(se_mask.size * error_rate)
+
+        if len(se_mask_error_free) < n_errors:
+            msg = f"The error rate of {error_rate} requires {len(se_mask_error_free)} error-free cells. "
+            msg += f"However, only {len(se_mask_error_free)} error-free cells are available."
+            raise ValueError(msg)
 
         # randomly choose error-cells
-        error_indices = np.random.default_rng(seed=self.seed).choice(se_data.size, n_errors, replace=False)
+        error_indices = np.random.default_rng(seed=self.seed).choice(se_mask_error_free.index, n_errors, replace=False)
         se_mask[error_indices] = True
         return error_mask
