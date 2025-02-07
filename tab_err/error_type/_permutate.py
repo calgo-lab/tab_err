98610895cbf4cfcd33f77a349c654957a3af265c
Nicholas Chandler <chandln@wwu.edu>
2025-02-07 13:44:22 +0100

    Refactor of _permutate.py

--- tab_err/error_type/_permutate.py
+++ tab_err/error_type/_permutate.py
@@ -13,16 +13,22 @@ if TYPE_CHECKING:
     import pandas as pd
 
 
-def generate_shuffle_pattern(format_len: int) -> list[int]:
+def _generate_shuffle_pattern(format_len: int) -> list[int]:
     """Generates a list of integers that indicates the positions of each value in a formatted string."""
     initial_pattern = list(range(format_len + 1))  # list that indicates the positions of each value
     new_pattern = initial_pattern
 
-    while initial_pattern == new_pattern:
+    while initial_pattern == new_pattern:  # Ensure the sample is different from original
         new_pattern = random.sample(initial_pattern, len(initial_pattern))
 
     return new_pattern
 
+def _check_column_format_consistency(separator_counts: list[int], column: int | str) -> None:
+    """Checks that each string in the column has the same number of separators, throws a ValueError if not."""
+    if len(set(separator_counts)) > 1:
+        msg = f"Column '{column}' cannot be permutated using a fixed permutation_automation_pattern: A fixed permutation_automation_pattern requires "
+        msg += "all values to be formatted in the same way."
+        raise ValueError(msg)
 
 class Permutate(ErrorType):
     """Permutates the parts of a compound value in a column."""
@@ -34,6 +40,22 @@ class Permutate(ErrorType):
         if not is_string_dtype(series):
             msg = f"Column {column} does not contain values of the string dtype. Cannot Permutate values."
             raise TypeError(msg)
+
+    def _random_pattern_function(self: Permutate, old_string: str) -> str:
+        """Generates a random permutation of 'old_string' elements split on the 'permutation_separator'."""
+        old_list = old_string.split(self.config.permutation_separator)
+        new = old_list
+        while new == old_list:  # Ensure the sample is different from original
+            new = random.sample(old_list, len(old_list))
+
+        return self.config.permutation_separator.join(new)
+
+    def _fixed_pattern_function(self: Permutate, old_string: str, new_pattern: list[int]) -> str:
+        """Generates a permutation of 'old_string' elements split on the 'permutation_separator' given a permutation specified by 'new_pattern'."""
+        string_as_part_lists = old_string.split(self.config.permutation_separator)
+        new_string_as_part_list = [string_as_part_lists[index] for index in new_pattern]
+
+        return self.config.permutation_separator.join(new_string_as_part_list)
 
     def _apply(self: Permutate, data: pd.DataFrame, error_mask: pd.DataFrame, column: int | str) -> pd.Series:
         """Applies the Permutate ErrorType to a column of data.
@@ -60,38 +82,17 @@ class Permutate(ErrorType):
                 msg += f'"{self.config.permutation_separator}". To use another separator, define it in the ErrorTypeConfig.'
                 raise ValueError(msg)
 
-        if self.config.permutation_pattern is not None or self.config.permutation_automation_pattern == "fixed":
-            if len(set(separator_counts)) > 1:
-                msg = f"Column {column} cannot be permutated using a fixed permutation_automation_pattern: A fixed permutation_automation_pattern requires "
-                msg += "all values to be formatted in the same way."
-                raise ValueError(msg)
+        if self.config.permutation_pattern is not None:  # Permutation of each entry from pattern.
+            _check_column_format_consistency()
+            new_pattern = self.config.permutation_pattern
+            series.loc[series_mask] = series.loc[series_mask].apply(self._fixed_pattern_function, args=(new_pattern,))
 
-            if self.config.permutation_pattern is not None:
-                new_pattern = self.config.permutation_pattern
+        elif self.config.permutation_automation_pattern == "fixed":  # Fixed permutation -- random once, applied to all.
+            _check_column_format_consistency()
+            new_pattern = _generate_shuffle_pattern(separator_counts[0])
+            series.loc[series_mask] = series.loc[series_mask].apply(self._fixed_pattern_function, args=(new_pattern,))
 
-            elif self.config.permutation_automation_pattern == "fixed":
-                new_pattern = generate_shuffle_pattern(separator_counts[0])
+        else:  # Random permutation -- random for each entry.
+            series.loc[series_mask] = series.loc[series_mask].apply(self._random_pattern_function)
 
-            # TODO(anyone): new_pattern possible unbound
-
-            def fixed_shuffle_pattern(old_string: str) -> str:
-                string_as_part_lists = old_string.split(self.config.permutation_separator)
-                new_string_as_part_list = [string_as_part_lists[index] for index in new_pattern]
-
-                return self.config.permutation_separator.join(new_string_as_part_list)
-
-            series.loc[series_mask] = series.loc[series_mask].apply(fixed_shuffle_pattern)
-            return series
-
-        # TODO(seja): quick-fix to avoid 'random_shuffle_pattern' being unbound, removing the following line
-        # if self.config.permutation_automation_pattern == "random":
-        def random_shuffle_pattern(old_string: str) -> str:
-            old_list = old_string.split(self.config.permutation_separator)
-            new = old_list
-            while new == old_list:
-                new = random.sample(old_list, len(old_list))
-
-            return self.config.permutation_separator.join(new)
-
-        series.loc[series_mask] = series.loc[series_mask].apply(random_shuffle_pattern)
         return series
